416133baac6006551f4acca3d4fd6678b3953758
lukemarkwordtlibertyits <luke.markwordt@libertyits.com>
2021-10-13 16:11:55 -0600

    health method shouldnt throw any errors

--- emis-client-v1/src/main/java/gov/va/api/lighthouse/emis/EmisVeteranStatusServiceClient.java
+++ emis-client-v1/src/main/java/gov/va/api/lighthouse/emis/EmisVeteranStatusServiceClient.java
@@ -2,11 +2,10 @@ package gov.va.api.lighthouse.emis;
 
 import gov.va.viers.cdi.emis.requestresponse.v1.EMISveteranStatusResponseType;
 import gov.va.viers.cdi.emis.requestresponse.v1.InputEdiPiOrIcn;
-import java.net.MalformedURLException;
 import org.springframework.http.ResponseEntity;
 
 public interface EmisVeteranStatusServiceClient {
-  ResponseEntity<String> health() throws MalformedURLException;
+  ResponseEntity<String> health();
 
   EMISveteranStatusResponseType veteranStatusRequest(InputEdiPiOrIcn ediPiOrIcn);
 }
