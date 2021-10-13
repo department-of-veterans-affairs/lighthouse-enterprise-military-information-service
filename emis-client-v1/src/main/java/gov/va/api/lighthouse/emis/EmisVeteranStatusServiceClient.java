be73087602d822e9f4ec0133eda1973a488d288d
lukemarkwordtlibertyits <luke.markwordt@libertyits.com>
2021-10-13 16:06:18 -0600

    Health check method accounts for exceptions

--- emis-client-v1/src/main/java/gov/va/api/lighthouse/emis/EmisVeteranStatusServiceClient.java
+++ emis-client-v1/src/main/java/gov/va/api/lighthouse/emis/EmisVeteranStatusServiceClient.java
@@ -2,10 +2,11 @@ package gov.va.api.lighthouse.emis;
 
 import gov.va.viers.cdi.emis.requestresponse.v1.EMISveteranStatusResponseType;
 import gov.va.viers.cdi.emis.requestresponse.v1.InputEdiPiOrIcn;
+import java.net.MalformedURLException;
 import org.springframework.http.ResponseEntity;
 
 public interface EmisVeteranStatusServiceClient {
-  ResponseEntity<String> health();
+  ResponseEntity<String> health() throws MalformedURLException;
 
   EMISveteranStatusResponseType veteranStatusRequest(InputEdiPiOrIcn ediPiOrIcn);
 }
